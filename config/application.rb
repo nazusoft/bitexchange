102fa9e6da32735936197d05c86e5d7e8a014562
cauecjm <cauecjm@gmail.com>
2018-06-12 20:01:39 -0300

    application.rb: configurando timezone

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Bitexchange
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
+
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale = :'pt-BR'
   end
 end
