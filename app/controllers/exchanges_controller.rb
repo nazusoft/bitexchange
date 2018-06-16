bc70e7a6ec686d80143778d0300f42c2ba66effc
cauecjm <cauecjm@gmail.com>
2018-06-16 16:15:05 -0300

    exchanges_controller: buscando conversões para bitcoin

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -4,9 +4,17 @@ class ExchangesController < ApplicationController
   end
 
   def convert
-    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
+    money_value    = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
+    to_btc_value   = ExchangeBitcoinService.new(params[:source_currency], params[:amount]).currency_to_btc
+    from_btc_value = ExchangeBitcoinService.new(params[:source_currency], params[:amount]).currency_from_btc
 
-    render json: { "value": value }
+    render json: {
+      source_currency: params[:source_currency],
+      target_currency: params[:target_currency],
+      money_value:     money_value,
+      to_btc_value:    to_btc_value,
+      from_btc_value:  from_btc_value
+    }
   end
 
 end
