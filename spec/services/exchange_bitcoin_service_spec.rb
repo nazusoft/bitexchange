ff50846f230b25d5488f46c0021444d5d993e3d6
cauecjm <cauecjm@gmail.com>
2018-06-23 08:27:08 -0300

    spec/services/exchange_bitcoin_service_spec: refatoração e add teste 'from_btc'

--- spec/services/exchange_bitcoin_service_spec.rb
+++ spec/services/exchange_bitcoin_service_spec.rb
@@ -3,11 +3,21 @@ require 'json'
 require './app/services/exchange_bitcoin_service'
 
 describe 'Currency' do
-  it 'exchange' do
+
+  it 'to_btc' do
     amount = rand(0..9999)
-    res    = ExchangeBitcoinService.new("USD", amount).perform
+    res    = ExchangeBitcoinService.new("USD", amount).currency_to_btc
 
     expect(res.is_a? Numeric).to eql(true)
     expect(res != 0 || amount == 0).to eql(true)
   end
+
+  it 'from_btc' do
+    amount = rand(0..9999)
+    res    = ExchangeBitcoinService.new("USD", amount).currency_from_btc
+
+    expect(res.is_a? Numeric).to eql(true)
+    expect(res != 0 || amount == 0).to eql(true)
+  end
+
 end
